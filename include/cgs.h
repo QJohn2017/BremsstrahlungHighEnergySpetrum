5f083c75d6259163a6699c521eff034316113015
Carmelo Evoli <carmelo.evoli@gssi.it>
2020-05-21 12:47:41 +0200

    updated functions

--- include/cgs.h
+++ include/cgs.h
@@ -1,12 +1,20 @@
+// Copyright 2020 Carmelo Evoli (GSSI) - MIT License
+#ifndef INCLUDE_CGS_H_
+#define INCLUDE_CGS_H_
+
 namespace cgs {
-    const double cm = 1;
-    const double erg = 1;
-    const double eV = 1.6022e-12 * erg;
-    const double MeV = 1e6 * eV;
-    const double GeV = 1e3 * MeV;
-    const double TeV = 1e3 * GeV;
-    const double r_e = 2.81794e-13 * cm;
-    const double alpha_f = 1. / 137.;
-    const double electron_mass_c2 = 0.511 * MeV;
-    const double mbarn = 1e-27 * cm * cm;
-}
+
+const double cm = 1;
+const double erg = 1;
+const double eV = 1.6022e-12 * erg;
+const double MeV = 1e6 * eV;
+const double GeV = 1e3 * MeV;
+const double TeV = 1e3 * GeV;
+const double r_e = 2.81794e-13 * cm;
+const double alpha_f = 1. / 137.;
+const double electron_mass_c2 = 0.511 * MeV;
+const double mbarn = 1e-27 * cm * cm;
+
+}  // namespace cgs
+
+#endif  // INCLUDE_CGS_H_
